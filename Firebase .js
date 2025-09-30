0bb15d5be685d9c27a4a0cbe2b76158eeb8de9cd
Phumlanithe1st <phukushmrp@gmail.com>
2025-09-30 11:27:36 +0200

    Add files via upload

---
+++ Firebase .js
@@ -0,0 +1,13 @@
+// js/firebase.js
+// Import your Firebase config
+const firebaseConfig = {
+  apiKey: "AIzaSyB-4udUo-CrchxMIBioNqh...",
+  authDomain: "dordrecht-news.firebaseapp.com",
+  projectId: "dordrecht-news",
+  storageBucket: "dordrecht-news.appspot.com",
+  messagingSenderId: "100889465393",
+  appId: "1:100889465393:web:e2fb39b09..."
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
